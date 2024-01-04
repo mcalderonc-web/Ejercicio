df3522f18648ed990b1f31d791941dba0b561a08
mcalderonc-web <mcalderonc@sdis.gov.co>
2024-01-04 15:26:07 -0500

    Primera sesion de Programacion sin AppModules

---
+++ src/app/ejercicio1/ejercicio1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio1.component.html',
+  styleUrl: './ejercicio1.component.css'
+})
+export class Ejercicio1Component {
+    Tipo1  = 'Flexion'
+}
